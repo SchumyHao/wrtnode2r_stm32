05797d023a8ef6ba84b202ea8b1dbf275868752e
SchumyHao <bob-hjl@126.com>
2015-12-07 18:09:21 +0800

    Revert "remove cpp init when use mdk"
    This reverts commit 54c9385734a2a7fcb548e441ccb6782a5d2b2f97.
    
    MDK really need cpp init

--- applications/startup.c
+++ applications/startup.c
@@ -96,10 +96,8 @@ void rtthread_startup(void)
 
 int main(void)
 {
-#ifndef __CC_ARM
     /* C++ global class init */
     cplusplus_system_init();
-#endif
 
     /* disable interrupt first */
     rt_hw_interrupt_disable();
